beef1cefc3749b51b0fa7c63e3a2a94c54d05bad
Edwin3333 <114894728+Edwin3333@users.noreply.github.com>
2022-11-24 01:10:54 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,47 @@
+      <section id="about">
+          <div class="container-fluid py-5" id="qualification">
+              <div class="container">
+                  <div class="position-relative d-flex align-items-center justify-content-center">
+                      <h1 class="display-1 text-uppercase text-white" style="-webkit-text-stroke: 1px #dee2e6;">Quality</h1>
+                      <h1 class="position-absolute text-uppercase text-primary">Education & Expericence</h1>
+                  </div>
+                  <div class="row align-items-center">
+                      <div class="col-lg-6">
+                          <h3 class="mb-4">My Education</h3>
+                          <div class="border-left border-primary pt-2 pl-4 ml-2">
+                              <div class="position-relative ex-me mb-4">
+                                  <i class="far fa-dot-circle text-primary position-absolute" style="top: 2px; left: -35px; font-size: 22px;"></i>
+                                  <h5 class="font-weight-bold mb-1">Bachelor of Science in Mechanical Engineering</h5>
+                                  <p class="mb-2"><strong>Polytechnic University of the Philippines</strong> | <small>April 2006 - April 2011</small></p>
+                              </div>
+                              <div class="position-relative ex-me mb-4">
+                                  <i class="far fa-dot-circle text-primary position-absolute" style="top: 2px; left: -35px;  font-size: 22px;"></i>
+                                  <h5 class="font-weight-bold mb-1">Full Stack Web Development</h5>
+                                  <p class="mb-2"><strong>Zuitt-Coding Bootcamp PH</strong> | <small>September 2022 - November 2022</small></p>
+                                  <p>A web development bootcamp focusing on both front-end and back-end technologies.
+                                Successfully created 3 capstone projects using the technology learned throughout the training</p>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-lg-6">
+                          <h3 class="mb-4">Work Expericence (Non-IT Field)</h3>
+                          <div class="border-left border-primary pt-2 pl-4 ml-2">
+                              <div class="position-relative ex-me mb-4">
+                                  <i class="far fa-dot-circle text-primary position-absolute" style="top: 2px;left: -35px;  font-size: 22px;"></i>
+                                  <h5 class="font-weight-bold mb-1">Plant Supervisor</h5>
+                                  <p class="mb-2"><strong>FCTEC Philippines Inc.</strong> | </p>
+                                  <p>Manufacturing of Automotive parts</p>
+                              </div>
+                              <div class="position-relative ex-me mb-4">
+                                  <i class="far fa-dot-circle text-primary position-absolute" style="top: 2px;left: -35px;  font-size: 22px;"></i>
+                                  <h5 class="font-weight-bold mb-1">Engineering Supervisor</h5>
+                                  <p class="mb-2"><strong>NT Philippines, Inc</strong> | </p>
+                                  <p>Manufacturing of Electronics Components</p>
+                              </div>
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+    </div>     
+      </section>
